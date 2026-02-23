9063c1e0d0265dbfdf5c784d5bcea5b96b167cf8
Mahdi Anjidani <>
2026-02-23 12:30:43 +0330

    branch

--- services/core/main.go
+++ services/core/main.go
@@ -10,7 +10,7 @@ func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
 	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
 	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	fmt.Printf("Hello and welcome, %s!\n", s, 12)
 
 	for i := 1; i <= 5; i++ {
 		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
